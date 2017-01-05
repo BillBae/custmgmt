bac2d06a629160bc341e28b669075f52ac0551a4
BillBae <baezzang1@naver.com>
2017-01-05 10:44:53 +0900

    chage2
    change2

--- Test1/Test1/Form1.cs
+++ Test1/Test1/Form1.cs
@@ -19,7 +19,7 @@ namespace Test1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("test");
+            MessageBox.Show("test 2222222222");
         }
     }
 }
